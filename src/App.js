ce485512e25c5ec3a57bd7f99084d774557a6e42
vishal gupta <vishalguptadishu@gmail.com>
2023-05-30 09:23:27 +0530

    twitter

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Singin from './component/singin';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Singin />
+    </div>
+  );
+}
+
+export default App;
